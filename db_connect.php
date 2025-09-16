206c11fa301375c3bbbf2db893de72b28f253673
theotriescoding <doralekka122@gmail.com>
2025-09-16 21:12:50 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,18 @@
 <?php
-$database_path = __DIR__ . '/car_rental.db';
+$host = 'localhost';
+$db = 'car_rental_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
-$dsn = "sqlite:$database_path";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
+
 try {
-     $pdo = new PDO($dsn, null, null, $options);
-     $pdo->exec("PRAGMA foreign_keys = ON"); // Enable foreign key constraints in SQLite
+     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
